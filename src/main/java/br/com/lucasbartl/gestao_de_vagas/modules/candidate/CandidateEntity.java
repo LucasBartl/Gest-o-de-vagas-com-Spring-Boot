3acff4d40dfc97aa4f684085d85512abd0676add
Lucas Rodrigues <lucas.bartllb@gmail.com>
2026-06-15 16:10:33 -0300

    Colocando validacoes iniciais

--- src/main/java/br/com/lucasbartl/gestao_de_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/lucasbartl/gestao_de_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,10 @@ package br.com.lucasbartl.gestao_de_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 /* 
@@ -13,9 +17,16 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não podem conter espaço")
     private String userName;
+
+    @Email(message = "O campo deve conter um email valido")
     private String email;
+    
+    @Length(min = 10, max = 50 )
     private String password;
+
     private String description;
     private String curriculum;
 
--- src/main/java/br/com/lucasbartl/gestao_de_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/lucasbartl/gestao_de_vagas/modules/candidate/controllers/CandidateController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lucasbartl.gestao_de_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class CandidateController {
 
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidate){
+    //@Valid vai pegar as regra de validaçoes que criamos dentro do CandidateEntity e só 
+    //recebera os valores que seguem a regra 
+    public void create(@Valid @RequestBody CandidateEntity candidate){
         System.out.println("Candidato:");
         System.out.println(candidate.getEmail());
         
