90ee3ff54393b9287c9dec9233dd255ed206fa4a
Lucas Rodrigues <lucas.bartllb@gmail.com>
2026-06-22 12:59:19 -0300

    Finalizando rotas publicas

--- src/main/java/br/com/lucasbartl/gestao_de_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/lucasbartl/gestao_de_vagas/security/SecurityConfig.java
@@ -5,18 +5,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-
-//Informando para o Spring que se trata de uma classe de
-//  configuração que queremos que ele gerencie 
+/*
+    *Informando para o Spring que se trata de uma classe de 
+    *configuração que queremos que o proprio Spring gerencie
+ */
 @Configuration
-
 public class SecurityConfig {
-    //Colocamos o Bean para indicar que é um objeto gerenciado pelo String
+    // *Colocamos o Bean para indicar que é um objeto gerenciado pelo String
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.csrf(csrf -> csrf.disable());
+       //*Desabilitando o SpringSecurity
+        http.csrf(csrf -> csrf.disable())
+        //*Definindo rotas autenticadas
+          .authorizeHttpRequests(auth -> {
+
+            //*Rotas definidas como autenticadas */
+            auth.requestMatchers("/candidate/").permitAll()
+            .requestMatchers("/company/").permitAll();
+            
+            //! Não autenticadas
+            auth.anyRequest().authenticated();
+
+        })
+        ;
         return http.build();
     }
-
 
 }
