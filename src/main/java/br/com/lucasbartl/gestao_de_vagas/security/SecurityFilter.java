245a70ba78edf791b9f71bcc584eec9bd9ce401b
Lucas Rodrigues <lucas.bartllb@gmail.com>
2026-06-23 14:31:27 -0300

    Ainda manipulando tokens de auth

--- src/main/java/br/com/lucasbartl/gestao_de_vagas/security/SecurityFilter.java
+++ src/main/java/br/com/lucasbartl/gestao_de_vagas/security/SecurityFilter.java
@@ -1,8 +1,13 @@
 package br.com.lucasbartl.gestao_de_vagas.security;
 
+import br.com.lucasbartl.gestao_de_vagas.Providers.JWTProvider;
 import br.com.lucasbartl.gestao_de_vagas.exceptions.ExceptionHandlerController;
 import java.io.IOException;
+import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,11 +20,9 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
-    private final ExceptionHandlerController exceptionHandlerController;
-
-    SecurityFilter(ExceptionHandlerController exceptionHandlerController) {
-        this.exceptionHandlerController = exceptionHandlerController;
-    }
+    // Pedindo para o spring instaciar
+    @Autowired
+    private JWTProvider jwtProvider;
 
     @Override
     protected void doFilterInternal(
@@ -28,13 +31,36 @@ public class SecurityFilter extends OncePerRequestFilter {
             FilterChain filterChain) // Filtro para validaçoes
             throws ServletException, IOException {
 
+        //Setando contexto do usuário como nulo
+        SecurityContextHolder.getContext().setAuthentication(null);
+
         // Coletando o header
         String header = request.getHeader("Authorization");
 
         System.out.println(header);
 
+        // Validacoes
+
+        if (header != null) {
+            // Validando o que temos dento do header
+            var subjectToken = this.jwtProvider.validateToken(header);
+
+            // Se vazio
+            if (subjectToken.isEmpty()) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+            request.setAttribute("company_id", subjectToken);
+            //inserindo usuário
+            UsernamePasswordAuthenticationToken auth =
+            new UsernamePasswordAuthenticationToken(subjectToken, null, Collections.emptyList());
+            //Injetando auth no springSecurity
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
+        }
+
         filterChain.doFilter(request, response);
-        
+
         throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
     }
 
